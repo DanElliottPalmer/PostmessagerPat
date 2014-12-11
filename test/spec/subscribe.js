3e5c9923c49f66b7970903021e0c38e57ec2d5f7
DanElliottPalmer <danelliottpalmer@gmail.com>
2014-12-11 16:10:00 +0000

    Add: tests for checking if the message came from a window

--- test/spec/subscribe.js
+++ test/spec/subscribe.js
@@ -69,3 +69,74 @@ test( "Multiple domains", function( assert ){
 	}
 
 } );
+
+test( "Target frame", function( assert ){
+
+	assert.expect( 1 );
+
+	var pp = new PostmessagerPat();
+	var done = assert.async();
+	var tmr = null;
+	var frame = document.getElementById("frameChild");
+
+	pp.subscribe( ALLOWED_ORIGIN, function( e ){
+		assert.equal( e.data, "true" );
+		clearTimeout( tmr ) && ( tmr = null );
+		complete();
+	}, frame.contentWindow );
+
+	addEvent( frame, "load", onFrameLoad );
+	frame.src = "./frames/subscribe.html";
+
+	function complete(){
+		removeEvent( frame, "load", onFrameLoad );
+		pp.destroy();
+		pp = null;
+		done();
+	}
+
+	function onFrameLoad(){
+		removeEvent( frame, "load", onFrameLoad );
+		tmr = setTimeout(function(){
+			assert.ok(false);
+			complete();
+		},500);
+	}
+
+} );
+
+test( "Disallowed frame", function( assert ){
+
+	assert.expect( 1 );
+
+	var pp = new PostmessagerPat();
+	var done = assert.async();
+	var tmr = null;
+	var frame = document.getElementById("frameChild");
+	var disallowedFrame = document.getElementById("frameDisallowed");
+
+	pp.subscribe( ALLOWED_ORIGIN, function( e ){
+		assert.ok(false);
+		clearTimeout( tmr ) && ( tmr = null );
+		complete();
+	}, frame.contentWindow );
+
+	addEvent( disallowedFrame, "load", onFrameLoad );
+	disallowedFrame.src = "./frames/subscribe.html";
+
+	function complete(){
+		removeEvent( disallowedFrame, "load", onFrameLoad );
+		pp.destroy();
+		pp = null;
+		done();
+	}
+
+	function onFrameLoad(){
+		removeEvent( disallowedFrame, "load", onFrameLoad );
+		tmr = setTimeout(function(){
+			assert.ok(true);
+			complete();
+		},500);
+	}
+
+} );
